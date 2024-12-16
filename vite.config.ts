efa56df2a9e40227fe8f74eac48f894959de294f
yk-lab <yk-lab@users.noreply.github.com>
2024-12-16 23:56:12 +0900

    fix(pdf): add cMapUrl for PDF.js to improve rendering

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,21 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-  ],
+    viteStaticCopy({
+      targets: [
+        {
+          src: './node_modules/pdfjs-dist/cmaps',
+          dest: 'assets/'
+        }
+      ]
+    })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
